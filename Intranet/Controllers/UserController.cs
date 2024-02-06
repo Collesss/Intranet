cdf553c849d2e794a69329d87d63c68e1f67e3c3
Collesss <sergey_lesikov@mail.ru>
2024-02-07 00:58:58 +0300

    Init

---
+++ Intranet/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Intranet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Intranet.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewUser()
+        { 
+            return View(new UserViewModel() { Name = User.Identity.Name, Email = "test@test.ru", WorkPhone="9009" }); 
+        }
+
+        public IActionResult ViewUsers()
+        {
+            return View();
+        }
+    }
+}
---
+++ Intranet/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Intranet.Models
+{
+    public class UserViewModel
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string WorkPhone { get; set; }
+    }
+}
